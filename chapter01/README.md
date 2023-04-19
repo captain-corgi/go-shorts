65befd7dd270e29a0ac1310a75b4c8652232d6bc
anh_tuan.l <anh_tuan.l@kbtg.tech>
2023-04-19 16:51:35 +0700

    Added chapter 01

---
+++ chapter01/README.md
@@ -0,0 +1,3 @@
+# Tebeka Shorts: Mutate a [ URL ] with Go
+
+[Ardan Lab Youtube](https://www.youtube.com/@ardanlabs6339)
--- chapter01/main.go
+++ chapter01/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/url"
+)
 
 func main() {
-	fmt.Println("Hello Chapter 1")
+	s := "https://google.com/search?q=rush&safe=active"
+
+	u, err := url.Parse(s)
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+
+	q := u.Query()
+	q.Set("q", "golang")
+	u.RawQuery = q.Encode()
+	fmt.Println(u.String())
+
+	// Expected result: https://google.com/search?q=golang&safe=active
 }
